5f919ae33292fc503dd2cca6aaf8f0921fcbaa9d
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-03-26 22:55:34 +0530

    adding basic questions

--- variables/variables.js
+++ variables/variables.js
@@ -34,18 +34,39 @@
 
 
 // 2. let is also a way to create variables but it is block scoped and it doesnot get added to window object in browser
-//1
-//  let a = 10;
-//  a= 20;
-// //  let a =90;            //  error----cant redeclare
-//  console.log(a)                 // output 20  
+        //1
+        //  let a = 10;
+        //  a= 20;
+        // //  let a =90;            //  error----cant redeclare
+        //  console.log(a)                 // output 20  
 
 
-//2. 
-// {
-//     let a = 10;
-//     console.log(a);           // output 10  
-    
-// }
+        //2. 
+        // {
+        //     let a = 10;
+        //     console.log(a);           // output 10  
+            
+        // }
 
-// console.log(a);            // error
+        // console.log(a);            // error
+
+
+//3. const is also a way to create variables but it is block scoped and it doesnot get added to window object in browser and it cant be reassigned and cant be redeclared
+// const is created for values which will not in entire program like value of pi i.e. 3.14
+
+        // //1
+        // const a = 10           // cant reassign and cant redeclare
+        // //a= 20                 error
+        // //const a = 90;         error
+        // console.log(a);        //output = 10
+        
+
+
+        //2. 
+        // {
+        //     const b = 50 ;
+        //     console.log(b);         // output 50
+            
+        // }
+
+        // console.log(b);             // error
---
+++ variables/variables_que_.js
@@ -0,0 +1,52 @@
+// 🟢 Basic
+// Create a variable a = 10 and print it.
+// Create two variables x = 5, y = 7 and print their sum.
+// Store your name in a variable and print: "My name is ___"
+// Declare a variable without value and print it.
+// Create a variable num = 20, change it to 50, and print it.
+
+
+// 🟡 Medium
+// Create two variables and swap their values.
+// Store length and breadth, find area of rectangle.
+// Store a number and check if it is even or odd.
+// Create variables for marks in 3 subjects and print total.
+// Create a variable and print its type using typeof.
+
+
+//Easy. 
+        // 1. Create a variable a = 10 and print it.
+        // let a = 10;
+        // console.log(a);
+
+        //2. Create two variables x = 5, y = 7 and print their sum.
+        // let x = 5;
+        // let y = 7;
+        // console.log("Sum is", x + y);
+
+        //3 Store your name in a variable and print: "My name is ___"
+        // let UserName ="Yuvraj Singh";
+        // console.log("My name is", UserName);
+        
+        //4 Declare a variable without value and print it.
+        // let val;
+        // console.log(val);                     output -->>   undefined because variable declared but not initialized or assigned any value
+        
+        //5. Create a variable num = 20, change it to 50, and print it.
+        // let num = 20 ;
+        // num = 50;
+        // console.log(num);
+      
+        
+//Medium
+        // Create two variables and swap their values.
+        // let a = 5;
+        // let b = 7;
+        // temp = a;
+        // a = b ;
+        // b = temp;
+
+        // console.log("value of a is ", a);
+        // console.log("value of b is " ,b);
+        
+        
