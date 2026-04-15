361f7c9203a5612d164cbdaaeb8240482d3120d4
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-04-15 21:31:40 +0530

    Understand concept of control_flow and solved  examples

---
+++ control_flow_statements.js/control_flow.js
@@ -0,0 +1,65 @@
+//Control Flow statements in javascript told the flow of program like if else statement, switch statement, for loop, while loop, do while loop, break statement, continue statement etc.
+
+//it generally tels in which side the program flows like if the condition is true then it will execute the block of code inside if statement otherwise it will execute the block of code inside else statement.
+
+//if else statement
+let age = 18;
+if (age > 18){
+    console.log("you are valid to vote");
+}
+else{
+    log("you are not valid to vote");
+}
+
+
+
+// switch statement
+let day = Number(prompt("Enter a number between 1 and 7:"));
+switch(day){
+    case 1 : console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid input! Please enter a number between 1 and 7.");
+}
+
+
+//Early return statement
+//early return statement is used to exit from a function before it reaches the end of the function. It is used to avoid unnecessary code execution and to improve the readability of the code.
+
+function grades(marks){
+    if (marks>= 100 && marks <90) return "A" ;
+    else if (marks >= 90 && marks < 70) return "B";
+    else if (marks >= 70 && marks < 60) return "C"; 
+    else if (marks >= 60 && marks < 50) return "D";
+    else if (marks >= 50 && marks < 40) return "E";
+    else if (marks >= 40 && marks < 0) return "F";
+    else return "Invalid marks! Please enter a number between 0 and 100.";
+}
+
+console.log(grades(85)); // Output: "B"
+console.log(grades(75));  // Output: "C"
+
+
+//2. example of early return statement
+function iseven(num){
+    if (num % 2 == 0 ) return true;
+    else return false;
+}
--- operators.js/logical_operators.js
+++ operators.js/logical_operators.js
@@ -1,21 +1,21 @@
-// //Logical operators are used to perform logical operations on operands and return a boolean value (true or false) based on the result of the operation. In JavaScript, there are three main logical operators: AND (&&), OR (||), and NOT (!).
+//Logical operators are used to perform logical operations on operands and return a boolean value (true or false) based on the result of the operation. In JavaScript, there are three main logical operators: AND (&&), OR (||), and NOT (!).
 
-// let x = true;
-// let y = false;  
+let x = true;
+let y = false;  
 
-// // Logical AND (&&) operator returns true if both operands are true, otherwise it returns false.
-// console.log(x && y); // false (both operands are not true)
+// Logical AND (&&) operator returns true if both operands are true, otherwise it returns false.
+console.log(x && y); // false (both operands are not true)
 
-// // Logical OR (||) operator returns true if at least one of the operands is true, otherwise it returns false.
-// console.log(x ||y); // true (at least one operand is true)
+// Logical OR (||) operator returns true if at least one of the operands is true, otherwise it returns false.
+console.log(x ||y); // true (at least one operand is true)
 
-// // Logical NOT (!) operator returns true if the operand is false, and returns false if the operand is true.
+// Logical NOT (!) operator returns true if the operand is false, and returns false if the operand is true.
 
-// console.log(!x); // false (x is true, so !x is false)
-// console.log(!y); // true (y is false, so !y is true)
+console.log(!x); // false (x is true, so !x is false)
+console.log(!y); // true (y is false, so !y is true)
 
-// // Logical operators can also be used with non-boolean values. In such cases, JavaScript uses "truthy" and "falsy" values to determine the result of the operation.
-// console.log(0 && x); // 0 (0 is falsy, so the result is 0)
-// console.log(1 || y); // 1 (1 is truthy, so the result is 1)
-// console.log(!0); // true (0 is falsy, so !0 is true)
+// Logical operators can also be used with non-boolean values. In such cases, JavaScript uses "truthy" and "falsy" values to determine the result of the operation.
+console.log(0 && x); // 0 (0 is falsy, so the result is 0)
+console.log(1 || y); // 1 (1 is truthy, so the result is 1)
+console.log(!0); // true (0 is falsy, so !0 is true)
 
