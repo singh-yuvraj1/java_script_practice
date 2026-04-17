83ae3aedba7dc9a50288920f4ca24d0c378307b9
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-04-17 15:20:03 +0530

    more discussion on loops

--- loops/basic_info.js
+++ loops/basic_info.js
@@ -6,7 +6,30 @@
 //2 while loop
 //3 do while loop
 
-// for loop
+//for loop is used when you know how many times you want to repeat a block of code.
+// it consist of three parts
+// 1. initialization
+// 2. condition
+// 3. increment/decrement
+
+
+// while loop is used when you don't know how many times you want to repeat a block of code.
+// it consist of two parts
+// 1. condition
+// 2. increment/decrement
+
+
+// do while loop is used when you want to execute a block of code at least once and then repeat it until a specified condition meet.
+// it consist of two parts
+// 1. block of code
+// 2. condition
+// in do while loop the block of code will be executed at least once even if the condition is false.
+
+
+//------------------------------Major differnece in for and while loop is that you can use for loop when
+//  you know how many times you want to repeat a block of code and you can use while loop when you don't know how many times you want to repeat a block of code.------------------------------ 
+
+// -----------------------------for loop------------------------------
 //1.
 for (let i = 0; i < 5; i++){
     console.log(i);
@@ -26,3 +49,44 @@ for (let i= 0; i<10; i++){
 
 ///output
 // Hello Duniya will be printed 10 times
+
+
+// -----------------------------while loop------------------------------
+//1.
+let i = 0;
+while (i < 5){
+    console.log(i);
+    i++;
+}
+//output
+// 0
+// 1
+// 2
+// 3
+// 4
+
+
+
+//-------------------------------do while loop------------------------------ 
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 5);
+//output
+// 0
+// 1
+// 2
+// 3
+// 4        
+
+
+//2.
+let k= 15;
+do{
+    console.log(k);
+    k++;
+}
+while(k<10);
+//output
+// 15
---
+++ loops/loops_que.js
@@ -0,0 +1,29 @@
+// 🔹 BASIC (Level 1)
+// Print numbers from 1 to 10
+// Print numbers from 10 to 1
+// Print all even numbers from 1 to 50
+// Print all odd numbers from 1 to 50
+// Find sum of numbers from 1 to N
+// Print multiplication table of a number (user input)
+// Count numbers from 1 to N that are divisible by 3
+// 🔹 LOGIC BUILDING (Level 2)
+// Find factorial of a number
+// Reverse a number
+// Count number of digits in a number
+// Find sum of digits
+// Check if number is palindrome
+// Find product of digits
+// Remove last digit repeatedly and print each step
+// 🔹 INTERMEDIATE (Level 3)
+// Print first N Fibonacci numbers
+// Check if a number is prime
+// Print all prime numbers from 1 to N
+// Find GCD of two numbers
+// Find LCM of two numbers
+// Check if number is Armstrong number
+// 🔹 LOOP CONTROL (Level 4)
+// Print numbers from 1–100 but:
+// Skip multiples of 5
+// Stop at 50
+// Find first number divisible by both 3 and 7 between 1–100
+// Print numbers until sum exceeds 100
