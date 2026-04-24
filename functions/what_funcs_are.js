9289729a27fa67d82f48e6bb07c00b034cca3230
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-04-24 22:42:24 +0530

    genearl syntax discussion on arrays

---
+++ functions/what_funcs_are.js
@@ -0,0 +1,51 @@
+// functions are reusable block of codes. we can use it as many times how many we want.
+// Function never run until we call it.
+// In javascipt functions can be created in three ways.
+//1. Function declaration/statement 
+//2. Function expression 
+//3. Fat arrow function 
+
+//1. Function declaration 
+//a
+function greet(){
+    console.log("Helolololo");
+}
+greet()    //function call  , helololo will no print until we write this line 
+
+
+//b. 
+
+function light_on(){
+    console.log("Light is turned on");
+}
+light_on()
+
+
+//2. Function expression
+//a.
+let greet1 = function(){
+    console.log("hlo sir, this is function expression way of writing function");
+}
+greet1()
+
+
+//b.
+let light_on1 =function(){
+    console.log("Light is turned on");
+}
+light_on()
+
+
+
+//3. arrow function 
+let greet2 = ()=>{
+    console.log("this is arrow function way to greet");
+}
+
+
+
+
+
+
+
+
