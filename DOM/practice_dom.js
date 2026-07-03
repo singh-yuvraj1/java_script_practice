20772b7d85f188730d84f7098376b7b38b85b29b
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-07-03 23:04:45 +0530

    dom practice again

---
+++ DOM/practice_dom.js
@@ -0,0 +1,20 @@
+let abc = document.getElementById("abc")
+abc.innerText = "i am starlight"
+console.dir(abc)
+
+
+let ab = document.getElementsByClassName(".ab")
+ab.textContent= "William Butcher"
+console.dir(ab)
+
+
+
+
+let bb = document.querySelector(".ab")
+bb.textContent = " soldier boy"
+console.dir(bb)
+
+
+let hh = document.querySelector("h1")
+hh.textContent = " soldier boy"
+console.dir(hh)
