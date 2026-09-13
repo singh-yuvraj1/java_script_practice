ec5169cc30bfeb131be6a03368569e2de55b34ac
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-09-13 22:50:03 +0530

    Learning promises

---
+++ async_js/promises.js
@@ -0,0 +1,19 @@
+// A promise is a type of code that will go into a state out of [resolve or reject] depending on situation of program but will go in 1 at a time , but still we have to to write code for both states
+
+
+let promise = new Promise(function(res , rej){
+    let rn = Math.floor(Math.random() * 10)
+    if (rn > 50) res(rn)
+        else rej(rn)
+})
+
+promise.then(function(val){
+    console.log(val);
+}).catch(function(val){
+    console.log(val);
+})
+
+//if res works then it will go inside "then"
+// if reject works then it will go inside "catch"
+
+
