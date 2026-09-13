80a75d635f7f960e90519ae363c9a9d867d8d7a4
singh-yuvraj1 <yuvraj.singh.95928@gmail.com>
2026-09-13 22:54:29 +0530

    solving promises que

--- async_js/promises.js
+++ async_js/promises.js
@@ -1,19 +1,33 @@
 // A promise is a type of code that will go into a state out of [resolve or reject] depending on situation of program but will go in 1 at a time , but still we have to to write code for both states
 
 
-let promise = new Promise(function(res , rej){
-    let rn = Math.floor(Math.random() * 10)
-    if (rn > 50) res(rn)
-        else rej(rn)
-})
+// let promise = new Promise(function(res , rej){
+//     let rn = Math.floor(Math.random() * 10)
+//     if (rn > 50) res(rn)
+//         else rej(rn)
+// })
 
-promise.then(function(val){
-    console.log(val);
-}).catch(function(val){
-    console.log(val);
-})
+// promise.then(function(val){
+//     console.log(val);
+// }).catch(function(val){
+//     console.log(val);
+// })
 
 //if res works then it will go inside "then"
 // if reject works then it will go inside "catch"
 
 
+///--------Same code use setTimeout---------
+
+let pr = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let number= Math.floor(Math.random()*10)
+        if (number>50) resolve(number)
+        else reject(number)
+    },5000);
+})
+pr.then(function(value){
+    console.log(value);
+}).catch(function (value) {
+    console.log(value);
+})
